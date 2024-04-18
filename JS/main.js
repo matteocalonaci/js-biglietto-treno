6caff0aab2eed577cf7bd0b6d9e0d260884c6999
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-04-18 17:24:25 +0200

    Commit 7

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
-const age = prompt("Quanti anni hai?");
-console.log(`La mia età è: ${age}`);
+// const age = prompt("Quanti anni hai?");
+// console.log(`La mia età è: ${age}`);
 
-const kmPercorsi = prompt("Quanti km devi fare?");
+let kmPercorsi;
 console.log(`Devo fare: ${kmPercorsi} km`);
 
 const costPerKm = 0.21;
@@ -10,22 +10,26 @@ console.log(`Il prezzo intero è: ${basePrice}€`)
 
 let finalPrice = basePrice;
 
-if (age < 18) {
-    console.log(`Applico lo sconto del 20%`)
-    const finalPrice = basePrice * 0.8
-    console.log(`Sconto del 20%:`, finalPrice)
-} else if (age > 65) {
-    console.log(`Applico lo sconto del 40%`)
-    const finalPrice = basePrice * 0.6
-    console.log(`Sconto del 40%:`, finalPrice)
-}
-console.log(`Prezzo finale:`, finalPrice)
+// if (age < 18) {
+//     console.log(`Applico lo sconto del 20%`)
+//     const finalPrice = basePrice * 0.8
+//     console.log(`Sconto del 20%:`, finalPrice)
+// } else if (age > 65) {
+//     console.log(`Applico lo sconto del 40%`)
+//     const finalPrice = basePrice * 0.6
+//     console.log(`Sconto del 40%:`, finalPrice)
+// }
+// console.log(`Prezzo finale:`, finalPrice)
 
 document.getElementById("prezzo").innerHTML = finalPrice;
 
 let username;
 
-document.getElementById("genera").onclick = function () {
+const myButton = document.getElementById(`clickable-button`)
+
+
+
+document.getElementById("myButton").onclick = function () {
     username = document.getElementById("myName").value;
     document.getElementById("myUsername").textContent = `${username}`;
 
@@ -33,4 +37,7 @@ document.getElementById("genera").onclick = function () {
     console.log(kmPercorsi);
 }
 
+myButton.addEventListener(`click`, function () {
+    alert(`hai vinto`)
+})
 
